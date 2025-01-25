bd5d9976ddeaaf1d67ffdb88c52fa8af86cdb460
Pucho <martinezochandarenam@gmail.com>
2025-01-25 15:07:58 +0100

    add coverage config

--- src/Pages/Dashboard.test.jsx
+++ src/Pages/Dashboard.test.jsx
@@ -6,7 +6,7 @@ import { describe, it, expect } from 'vitest';
 import Dashboard from './Dashboard';
 
 describe("Dashboard components", ()=> {
-    it("", ()=>{
+    it("should show title", ()=> {
         
     })
 })
